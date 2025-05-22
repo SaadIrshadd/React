0194ade1fdd3785dede1368db1ef525da2d03542
Saad <saad2005irshadd@gmail.com>
2025-05-23 04:29:06 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
-import TextForm from "./components/TextForm";
+// import TextForm from "./components/TextForm";
 
 function App() {
   return (
     <>
       <Navbar title="Textutils" about="About" />
       
-      <div className="container my-3">
+      {/* <div className="container my-3">
         <TextForm heading="Enter the text to analize" />
-      </div>
+      </div> */}
+
+      <About/>
     </>
   );
 }
 
 export default App;
 
-//<></>this is a jsx or React fragment which allows you to return multiple elements.
+//<></>this is a jsx or React fragment which allows you to return multiple elements
