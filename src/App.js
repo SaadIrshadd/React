3be2c11be81f348743fed56fe5c5c78f6d34581d
Saad <saad2005irshadd@gmail.com>
2025-05-23 08:41:43 +0500

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,63 +1,73 @@
 import "./App.css";
 import Alert from "./components/Alert";
-// import About from "./components/About";
+import About from "./components/About";
 import Navbar from "./components/Navbar";
 import TextForm from "./components/TextForm";
-import React,{useState} from 'react'
-
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [DarkMode, setDarkMode] = useState('light')
-  const [alert, setAlert] = useState(null)
+  const [DarkMode, setDarkMode] = useState("light");
+  const [alert, setAlert] = useState(null);
 
-  const Switch= ()=>{
-    if(DarkMode==="light"){
-      setDarkMode("dark")
-      document.body.style.backgroundColor="black";
+  const Switch = () => {
+    if (DarkMode === "light") {
+      setDarkMode("dark");
+      document.body.style.backgroundColor = "black";
       AlertMsg("Dark mode has been enabled");
-      document.title="TextUtils - DarkMode"
-    }
-    else{
-      setDarkMode("light")
-      document.body.style.backgroundColor="white"
+      document.title = "TextUtils - DarkMode";
+    } else {
+      setDarkMode("light");
+      document.body.style.backgroundColor = "white";
       AlertMsg("Light mode has been enabled");
-      document.title="TextUtils - Home"
+      document.title = "TextUtils - Home";
     }
-  }
+  };
 
-  const secSwitch= ()=>{
-    if(DarkMode==="light"){
-      setDarkMode("dark")
-      document.body.style.backgroundColor="#091723";
+  const secSwitch = () => {
+    if (DarkMode === "light") {
+      setDarkMode("dark");
+      document.body.style.backgroundColor = "#091723";
       AlertMsg("Blue mode has been enabled");
-      document.title="TextUtils - BlueMode"
-    }
-    else{
-      setDarkMode("light")
-      document.body.style.backgroundColor="white"
+      document.title = "TextUtils - BlueMode";
+    } else {
+      setDarkMode("light");
+      document.body.style.backgroundColor = "white";
       AlertMsg("Light mode has been enabled");
-      document.title="TextUtils - Home"
+      document.title = "TextUtils - Home";
     }
-  }
+  };
 
   const AlertMsg = (message) => {
     setAlert(message);
     setTimeout(() => {
       setAlert(null);
     }, 1500);
-  }
+  };
 
   return (
     <>
-      <Navbar title="Textutils" about="About" mode={DarkMode} switchMode={Switch} secondSwitch={secSwitch}/>
+      <BrowserRouter>
+        <Navbar title="Textutils" about="About" mode={DarkMode} switchMode={Switch} secondSwitch={secSwitch}/>
 
-      <Alert alert={alert}/>
-      
-      <div className="container my-3">
-        <TextForm heading="Enter the text to analize" mode={DarkMode} AlertMsg={AlertMsg}/>
-      </div>
-
-      {/* <About/> */}
+        <Alert alert={alert} />
+        
+        <Routes>
+          <Route path="/about" element={<About />}>
+          </Route>
+          
+          <Route path="/" element={
+              <div className="container my-3">
+                <TextForm
+                  heading="Enter the text to analize"
+                  mode={DarkMode}
+                  AlertMsg={AlertMsg}
+                />
+              </div>
+            }
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React from "react";
 import PropTypes from "prop-types";
+import { Link } from 'react-router-dom'
 
 //It is another method of passing props you can also set default values to the props
 //It is called destructuring method
@@ -15,9 +16,9 @@ export default function Navbar({
     <div>
       <nav className={`navbar navbar-expand-lg navbar-${mode} bg-${mode}`}>
         <div className="container-fluid">
-          <a className="navbar-brand" href="/">
+          <Link className="navbar-brand" to="/">
             {title}
-          </a>
+          </Link>
           <button
             className="navbar-toggler"
             type="button"
@@ -32,14 +33,14 @@ export default function Navbar({
           <div className="collapse navbar-collapse" id="navbarSupportedContent">
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
-                <a className="nav-link active" aria-current="page" href="/">
+                <Link className="nav-link active" aria-current="page" to="/">
                   Home
-                </a>
+                </Link>
               </li>
               <li className="nav-item">
-                <a className="nav-link" href="/">
+                <Link className="nav-link" to="/about">
                   {about}
-                </a>
+                </Link>
               </li>
             </ul>
             {/* <form className="d-flex" role="search">
